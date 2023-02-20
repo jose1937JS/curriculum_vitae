8ffd866db0f1bd26c60494238fc670cfd1c924c6
Jose <jflo1937@gmail.com>
2023-02-20 14:42:59 -0400

    Nueva version en progreso

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,14 @@
 $(() => {
-
 	// Este script se encarga de inyectar las imagenes correspondiente del proyecto que el usuario quiere ver en el modal.
 
 	let imagenes = [
-		{
-			src: ['SE_Login.png', 'SE_Inicio.png', 'SE_Profesor_Contenido.png', 'SE_Profesor_Crear_Evaluacion.png', 'SE_Profesor_Evaluaciones.png', 'SE_Estudiante_Evaluacion.png', 'SE_Estudiante_Evaluacion_Resultado.png', 'SE_Profesor_Asignaciones.png', 'SE_Chat.png', 'SE_Perfil.png']
-		},
-		{
-			src: ['Aula_Virtual_Login.png', 'Aula_Virtual_Inicio.png', 'Aula_Virtual_Notas.png']
-		},
-		{
-			src: ['Fundapi_doctors.png', 'Fundapi_services.png', 'Fundapi_specialties.png', 'Fundapi_json.png']
-		},
-		{
-			src: ['Rosas_De_America_Inicio.jpg', 'Rosas_De_America_Productos.png', 'Rosas_De_America_Carrito_de_Compra.png']
-		},
-		{
-			src: ['SoftwareEducativo_2_Inicio.png']
-		},
-		{
-			src: ['CLAP_Login.png', 'CLAP_Inicio.png', 'CLAP_transferencias.png']
-		}
+		{ src: ['m2mapp_2.png', 'm2mapp_3.png', 'm2mapp_8.png', 'm2mapp_9.png', 'm2mapp_10.png', 'm2mapp_4.png', 'm2mapp_5.png', 'm2mapp_6.png', 'm2mapp_7.png']},
+		{ src: ['promartv.png']},
+		{ src: ['SE_Login.png', 'SE_Inicio.png', 'SE_Profesor_Contenido.png', 'SE_Profesor_Crear_Evaluacion.png', 'SE_Estudiante_Evaluacion.png', 'SE_Estudiante_Evaluacion_Resultado.png', 'SE_Profesor_Asignaciones.png', 'SE_Chat.png', 'SE_Perfil.png']},
+		{ src: ['Aula_Virtual_Login.png', 'Aula_Virtual_Notas.png']},
+		{ src: ['Fundapi_doctors.png', 'Fundapi_services.png', 'Fundapi_specialties.png', 'Fundapi_json.png']},
+		{ src: ['Rosas_De_America_Inicio.jpg', 'Rosas_De_America_Productos.png', 'Rosas_De_America_Carrito_de_Compra.png']},
+		{ src: ['CLAP_Login.png', 'CLAP_Inicio.png', 'CLAP_transferencias.png']}
 	];
 
 	$('.btn-dark').click(function(){
@@ -31,7 +19,6 @@ $(() => {
 		$('#indicators').html('')
 
 		for (let img of imagenes[pos].src) {
-			
 			if (i == 0) {
 				$('#indicators').append(`
 					<li data-target="#carouselExampleIndicators" data-slide-to="${i}" class="active"></li>
@@ -39,7 +26,14 @@ $(() => {
 
 				$('#imagenes').append(`
 					<div class="carousel-item active">
-						<img src="./assets/images/${img}" class="d-block w-100" alt="...">
+						<div class="w-100 d-flex justify-content-center">
+						
+							<img src="./assets/images/${img}" 
+								style="border: 2px solid black; border-radius: 5px;" 
+								class="img-fluid text-center" 
+								alt="No se encontró la imagen"
+							>
+						</div>
 					</div>
 				`);
 			}
@@ -50,7 +44,14 @@ $(() => {
 
 				$('#imagenes').append(`
 					<div class="carousel-item">
-						<img src="./assets/images/${img}" class="d-block w-100" alt="...">
+						<div class="w-100 d-flex justify-content-center">
+						
+							<img src="./assets/images/${img}" 
+								style="border: 2px solid black; border-radius: 5px;" 
+								class="img-fluid text-center" 
+								alt="No se encontró la imagen"
+							>
+						</div>
 					</div>
 				`);
 			}
@@ -58,7 +59,4 @@ $(() => {
 			i++
 		}
 	});
-
-
-
 });
