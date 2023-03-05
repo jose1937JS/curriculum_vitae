b4c304a0dec86ba914fa76bb16e8e0c144d87f6c
Jose <jflo1937@gmail.com>
2023-03-04 22:03:43 -0400

    Nueva version del CV/Portafolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,16 @@ $(() => {
 	// Este script se encarga de inyectar las imagenes correspondiente del proyecto que el usuario quiere ver en el modal.
 
 	let imagenes = [
-		{ src: ['m2mapp_2.png', 'm2mapp_3.png', 'm2mapp_8.png', 'm2mapp_9.png', 'm2mapp_10.png', 'm2mapp_4.png', 'm2mapp_5.png', 'm2mapp_6.png', 'm2mapp_7.png']},
-		{ src: ['promartv.png']},
-		{ src: ['SE_Login.png', 'SE_Inicio.png', 'SE_Profesor_Contenido.png', 'SE_Profesor_Crear_Evaluacion.png', 'SE_Estudiante_Evaluacion.png', 'SE_Estudiante_Evaluacion_Resultado.png', 'SE_Profesor_Asignaciones.png', 'SE_Chat.png', 'SE_Perfil.png']},
-		{ src: ['Aula_Virtual_Login.png', 'Aula_Virtual_Notas.png']},
-		{ src: ['Fundapi_doctors.png', 'Fundapi_services.png', 'Fundapi_specialties.png', 'Fundapi_json.png']},
-		{ src: ['Rosas_De_America_Inicio.jpg', 'Rosas_De_America_Productos.png', 'Rosas_De_America_Carrito_de_Compra.png']},
-		{ src: ['CLAP_Login.png', 'CLAP_Inicio.png', 'CLAP_transferencias.png']}
+		{ src: ['m2m/Screenshot_1677978167.png', 'm2m/m2mapp_2.png', 'm2m/Screenshot_1677978173.png', 'm2m/Screenshot_1677978404.png', 'm2m/m2mapp_3.png', 'm2m/m2mapp_8.png', 'm2m/m2mapp_9.png', 'm2m/Screenshot_1677978449.png', 'm2m/Screenshot_1677978495.png', 'm2m/m2mapp_10.png', 'm2m/m2mapp_4.png', 'm2m/m2mapp_5.png', 'm2m/m2mapp_6.png', 'm2m/m2mapp_7.png', 'm2m/Screenshot_1677978429.png']},
+		{ src: ['halaken/Screenshot_1677955415.png', 'halaken/Screenshot_1677955411.png', 'halaken/Screenshot_1677953899.png', 'halaken/Screenshot_1677953903.png', 'halaken/Screenshot_1677953936.png', 'halaken/Screenshot_1677953942.png', 'halaken/Screenshot_1677955298.png', 'halaken/Screenshot_1677955305.png', 'halaken/Screenshot_1677955318.png', 'halaken/Screenshot_1677955344.png', 'halaken/Screenshot_1677955388.png']},
+		{ src: ['miekipo/Screenshot_1677974901.png', 'miekipo/Screenshot_1677974972.png']},
+		{ src: ['promartv/promartv.png']},
+		{ src: ['tierraXtierra/Screenshot_1677958523.png', 'tierraXtierra/Screenshot_1677958526.png', 'tierraXtierra/Screenshot_1677958598.png', 'tierraXtierra/Screenshot_1677958609.png', 'tierraXtierra/Screenshot_1677958613.png', 'tierraXtierra/Screenshot_1677958668.png']},
+		{ src: ['SistemaEducativo/SE_Login.png', 'SistemaEducativo/SE_Inicio.png', 'SistemaEducativo/SE_Profesor_Contenido.png', 'SistemaEducativo/SE_Profesor_Crear_Evaluacion.png', 'SistemaEducativo/SE_Estudiante_Evaluacion.png', 'SistemaEducativo/SE_Estudiante_Evaluacion_Resultado.png', 'SistemaEducativo/SE_Profesor_Asignaciones.png', 'SistemaEducativo/SE_Chat.png', 'SistemaEducativo/SE_Perfil.png']},
+		{ src: ['AulaVirtual/Aula_Virtual_Login.png', 'AulaVirtual/Aula_Virtual_Notas.png']},
+		{ src: ['Fundapi/Fundapi_doctors.png', 'Fundapi/Fundapi_services.png', 'Fundapi/Fundapi_specialties.png', 'Fundapi/Fundapi_json.png']},
+		{ src: ['RosasDeAmerica/Rosas_De_America_Inicio.jpg', 'RosasDeAmerica/Rosas_De_America_Productos.png', 'RosasDeAmerica/Rosas_De_America_Carrito_de_Compra.png']},
+		{ src: ['Clap/CLAP_Login.png', 'Clap/CLAP_Inicio.png', 'Clap/CLAP_transferencias.png']}
 	];
 
 	$('.btn-dark').click(function(){
